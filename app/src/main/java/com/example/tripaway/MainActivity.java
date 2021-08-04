49b5ee75b4a1d8aa1a152dfca3c848ab35c1f077
Alaa-Sherif <allaashreef@gmail.com>
2021-08-05 00:04:25 +0200

    my first commit

--- app/src/main/java/com/example/tripaway/MainActivity.java
+++ app/src/main/java/com/example/tripaway/MainActivity.java
@@ -6,14 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    // My first Commit
     Button btnSignUp, btnLogin;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
