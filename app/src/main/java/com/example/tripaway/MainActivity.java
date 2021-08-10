6d78c1e93011f3cc8040359e226a03064e7854f8
WeaamMohamed <54490984+WeaamMohamed@users.noreply.github.com>
2021-08-10 23:14:27 +0200

    Edit2

--- app/src/main/java/com/example/tripaway/MainActivity.java
+++ app/src/main/java/com/example/tripaway/MainActivity.java
@@ -11,7 +11,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    //edit
+    //edit2
     Button btnSignUp, btnLogin;
     Button home;
     @Override
