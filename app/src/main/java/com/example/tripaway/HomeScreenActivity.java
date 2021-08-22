2aafac622fe1d2a95d00c879b13aeeb40c58be41
AlaaElmoro <alaa.m.elmor@gmail.com>
2021-08-22 04:51:30 +0200

    mail

--- app/src/main/java/com/example/tripaway/HomeScreenActivity.java
+++ app/src/main/java/com/example/tripaway/HomeScreenActivity.java
@@ -2,7 +2,9 @@ package com.example.tripaway;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -12,20 +14,31 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.tripaway.databinding.ActivityHomeScreenBinding;
+import com.example.tripaway.utils.FireStoreHelper;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class HomeScreenActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityHomeScreenBinding binding;
+    TextView txtEmail,txtUserName;
+    private static FirebaseFirestore dbFireStore;
+    private static FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        mAuth = FirebaseAuth.getInstance();
+        dbFireStore = FirebaseFirestore.getInstance();
         binding = ActivityHomeScreenBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        //setContentView(R.layout.nav_header_home_screen);
 
         setSupportActionBar(binding.appBarHomeScreen.toolbar);
         binding.appBarHomeScreen.fab.setOnClickListener(new View.OnClickListener() {
@@ -48,7 +61,22 @@ public class HomeScreenActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_home_screen);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        View headerView = navigationView.getHeaderView(0);
+        TextView navUsername = (TextView) headerView.findViewById(R.id.userName);
+        TextView navEmail = (TextView) headerView.findViewById(R.id.email);
 
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            navEmail.setText(user.getEmail());
+            String displayName = user.getDisplayName();
+            for (UserInfo userInfo : user.getProviderData()) {
+                if (displayName == null && userInfo.getDisplayName() != null) {
+                    displayName = userInfo.getDisplayName();
+                }
+            }
+//            navUsername.setText("Welcome "+displayName);
+            navUsername.setText("Tripawy ");
+        }
 
 
     }
